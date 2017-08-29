0a0893a61619870d4b1a6b71522be3cd43b10676
Le-Prof <proffxginfo@hotmail.com>
2017-08-29 19:07:43 -0400

    Ajout d'une nouvelle classe.

---
+++ bla/bla/bidule.cs
@@ -0,0 +1,29 @@
+﻿/******************************************************************************
+ * Classe:  Bidule
+ * 
+ * Auteur:  Le Prof
+ * 
+ * But:     Créer une classe bidon à ajouter à un projet distribué à l'aide de
+ *          Git.
+ *****************************************************************************/
+ 
+ namespace Machin
+{
+    public class Bidule
+    {
+        /// <summary>
+        /// Attribut servant à stoker quelque chose d'insignifiant.
+        /// </summary>
+        private int m_iKekChose;
+
+        /// <summary>
+        /// Constructeur servant à initialiser l'attribut lors de la création
+        /// d'un objet de la classe. L'attribut est initialisé arbitrairement
+        /// à la valeur 25.
+        /// </summary>
+        public Bidule ()
+        {
+            m_iKekChose = 25;
+        }
+    }
+}
