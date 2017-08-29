d797822fe10c0c38e30c4344ebe2f0977d7d47ce
Le-Prof <proffxginfo@hotmail.com>
2017-08-29 18:20:54 -0400

    Envoi initial

---
+++ bla/bla/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bonjour tout le monde!");
+        }
+    }
+}
